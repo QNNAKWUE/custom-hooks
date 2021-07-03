3dfc254b5f16b5509cea564a7da0d01a71ec7c69
Queen Nnakwue <qnnakwue@gmail.com>
2021-07-03 03:41:37 +0100

    making updates

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -1,26 +1,32 @@
 import React from 'react';
-import { useState } from 'react';
+import useInput from '../hooks/useInput';
 
 export default function UserForm() {
-    const [firstName, setfirstName] = useState('');
-    const [lastName, setlastName] = useState('');
+
+    const [firstName, bindFirstName, resetFirstName] = useInput('');
+     const [lastName, bindLastName, resetLastName] = useInput('')
+
+    const submitHandler = e => {
+        e.preventDefault();
+        alert(`Hello ${firstName} ${lastName}`);
+        resetFirstName();
+        resetLastName();
+    }
 
     return(
         <>
-            <form>
+            <form onSubmit={submitHandler}>
                 <div>
                     <label>First Name</label>
                     <input 
-                    value={firstName} 
-                    onChange={e =>setfirstName(e.target.value)} 
+                    {...bindFirstName}
                     type='text' />
                 </div>
 
                 <div>
                     <label>Last Name</label>
                     <input 
-                    value={lastName} 
-                    onChange={e =>setlastName(e.target.value)}
+                    {...bindLastName}
                     type='text' />
                 </div>
 
